a1f8ec8974de37b6cd3f78d799a733d78b9f216d
another-dark-knight <spandanghosh2@gmail.com>
2020-03-29 00:18:20 +0530

    Prep script ready

--- create_data.py
+++ create_data.py
@@ -24,18 +24,26 @@ i=0
 k=0
 wordpath=None
 a = len(words)
+sample = False
+
 while True and i<a:
     if i==0:
         wordpath = os.path.join(gesturepath,words[i])
         makedir(wordpath)
         k = len(os.listdir(wordpath))     
     ret, frame = cap.read()
+    
+    if k!=0 and sample is False:
+        x = cv2.imread(os.path.join(gesturepath,words[i],"0.png"))
+        cv2.imshow("sample", x)
+        sample = True
+
     frame = cv2.resize(frame,(600,600))
     cv2.rectangle(frame,start,end,(0,0,255),2)
     hand = cv2.resize(frame[starty:endy, startx:endx], (300,300))
     hand2 = cv2.flip(hand,1)
     frame = cv2.flip(frame,1)
-    cv2.putText(frame,"Record " + words[i],10,500, cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0))
+    cv2.putText(frame,"Record: " + words[i],(190,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0), 6)
     cv2.imshow("stream", frame)
     cv2.imshow("hand",hand2)
     key = cv2.waitKey(1) & 0xFF
@@ -51,6 +59,7 @@ while True and i<a:
         try:
             wordpath = os.path.join(gesturepath,words[i])
             makedir(wordpath)
+            sample = False
             k=len(os.listdir(wordpath))
         except:
             print("Words over!")
